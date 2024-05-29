fdd8a052d617719fc896a20851b42b80a005f1cb
OnoTakumi <oibc35120@ojs.ac.jp>
2024-05-29 10:02:56 +0900

    第4章処理

--- Chapter04/Section01/Program.cs
+++ Chapter04/Section01/Program.cs
@@ -26,15 +26,24 @@ namespace Section01 {
             Console.WriteLine(ret);*/
             #endregion
 
-
+            //↓例外処理
             Console.WriteLine("整数を入力");
             string inputNum = Console.ReadLine();
-            int num;
 
-            if (int.TryParse(inputNum, out num)) {
-                Console.WriteLine("整数に変換した値 :" + num);
-            } else {
-                Console.WriteLine("違うだろ!?違うだろ!?:");
+            try {
+                int num = int.Parse(inputNum);
+            }
+            catch (FormatException ex) {
+                Console.WriteLine("FormatException" + ex.Message);    
+            }
+            catch(ArgumentException ex) {
+                Console.WriteLine("ArgumentException" + ex.Message);
+            }
+            catch (OverflowException ex) {
+                Console.WriteLine("OverflowException" + ex.Message);
+            }
+            finally {
+                Console.WriteLine("処理が終了");
             }
 
         }
