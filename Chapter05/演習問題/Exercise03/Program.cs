0c0f2ba826b80af47870a5c31cb308fad8324667
OnoTakumi <oibc35120@ojs.ac.jp>
2024-05-30 14:53:50 +0900

    Chapter055.3.3まで

---
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Jackdaws love my big sphinx of quartz";
+
+            Exercise3_1(text);
+            Console.WriteLine("-----");
+
+            Exercise3_2(text);
+            Console.WriteLine("-----");
+
+            Exercise3_3(text);
+            Console.WriteLine("-----");
+
+            Exercise3_4(text);
+            Console.WriteLine("-----");
+
+            Exercise3_5(text);
+        }
+
+        private static void Exercise3_1(string text) {
+            var spaces = text.Count(c => c == ' ');
+            Console.WriteLine("空白数:{0}", spaces);
+        }
+
+        private static void Exercise3_2(string text) {
+            var replaced = text.Replace("big", "small");
+        }
+
+        private static void Exercise3_3(string text) {
+            var count = 0;
+            var loopCount = text.Length;
+            for (var i = 0; i < loopCount; i++) {
+                if (text.Substring(i, text.Length) == text) { 
+
+                    Console.WriteLine( count++ );
+                }
+            }
+            
+        }
+
+        private static void Exercise3_4(string text) {
+            
+        }
+
+        private static void Exercise3_5(string text) {
+            
+        }
+    }
+}
