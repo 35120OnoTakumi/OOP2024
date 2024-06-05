a78f7c57e4bb960e3c82927c6b110d6cd1c79c98
OnoTakumi <oibc35120@ojs.ac.jp>
2024-06-05 12:05:24 +0900

    第６章

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var books = Books.GetBooks().OrderByDescending(x => x.Price).ToList();
+
+            books.ForEach(x => Console.WriteLine(x.Title + ":" + x.Price));
+
+            /*
+            foreach (var book in books) {
+                Console.WriteLine(book.Title + ":" + book.Price);
+            }
+            */
+        }
+    }
+}
