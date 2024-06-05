6cfda63ac1c517ceb85b968ce9283de729d53c97
OnoTakumi <oibc35120@ojs.ac.jp>
2024-06-05 14:03:03 +0900

    第６章演習問題完成

--- Chapter06/演習問題/Exercise01/Program.cs
+++ Chapter06/演習問題/Exercise01/Program.cs
@@ -26,23 +26,37 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_1(int[] numbers) {
-            
+            var max = numbers.Max();
+            Console.WriteLine(max);
         }
 
-        private static void Exercise1_2(int[] numbers) {
-            
+        private static void Exercise1_2(int[] numbers) {            
+            var lastN = numbers.Skip(numbers.Length - 2).ToArray();
+            foreach (int n in lastN) {
+                Console.WriteLine(n);
+            }
+
+
         }
 
-        private static void Exercise1_3(int[] numbers) {
-            
+        private static void Exercise1_3(int[] numbers) {            
+            var str = numbers.Select(n => n.ToString()).ToArray();
+            foreach(var num in str)
+            Console.WriteLine(num);
         }
 
         private static void Exercise1_4(int[] numbers) {
-            
+            var num1 = numbers.OrderBy(n => n).ToArray().Where(n => n > 0).Take(3);
+            foreach(var n in num1) {
+                Console.WriteLine(n);
+            }
+
         }
 
         private static void Exercise1_5(int[] numbers) {
-            
+            var results = numbers.Distinct().ToArray();
+            var count = results.Count(n => n > 10);
+            Console.WriteLine(count);
         }
     }
 }
